379388a5341b7a34715a82d323df253ff261a7e4
Simon Bahamonde <simon.bahamonde@local.dts.cl>
2024-12-13 14:56:34 -0300

    Finished section 188: Lag Compensation Component

---
+++ Source/Blaster/Private/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterComponents/LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
