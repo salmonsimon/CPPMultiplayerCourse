177635cd9e2b52582e6743812cc7ade84387cc3e
salmonsimon <38538239+salmonsimon@users.noreply.github.com>
2025-07-17 09:12:04 -0400

    Dont know what this is

---
+++ Source/Blaster/Private/HUD/EliminationAnnouncement.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/EliminationAnnouncement.h"
+
+void UEliminationAnnouncement::SetEliminationAnnouncementText(FString AttackerName, FString VictimName)
+{
+
+}
---
+++ Source/Blaster/Public/HUD/EliminationAnnouncement.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EliminationAnnouncement.generated.h"
+
+class UHorizontalBox;
+class UTextBlock;
+
+UCLASS()
+class BLASTER_API UEliminationAnnouncement : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public: 
+
+	void SetEliminationAnnouncementText(FString AttackerName, FString VictimName);
+
+	UPROPERTY(meta = (BindWidget))
+	UHorizontalBox* AnnouncementBox;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* AnnouncementText;
+};
